b01e22e360db6752671b13aa76b9765467245ada
christian <cfs.bsi@outlook.com>
2018-05-08 04:53:48 -0300

    feat: Add Animation on deck click

--- components/Deck.js
+++ components/Deck.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import styled from 'styled-components/native';
+import React, {Component} from 'react'
+import styled from 'styled-components/native'
+import {Animated} from 'react-native'
 import {cardQuantitiesLiteral} from '../utils/helpers'
 
 
-export default function Deck(props) {
-    const {deck} = props;
+export default class Deck extends Component {
 
-    return (
-        <DeckStyled>
-            <Title onPress={() => props.navigation.navigate('DeckInfo', {deck})} >{deck.title}</Title>
-            <Subtitle>{cardQuantitiesLiteral(deck.questions.length)}</Subtitle>
-        </DeckStyled>
-    )
+    constructor(props){
+        super(props)
+
+        this.state = {fontSize: new Animated.Value(1)}
+    }
+
+    titlePressed = () => {
+        const {fontSize} = this.state
+        const {deck} = this.props;
+
+        Animated.sequence([
+          Animated.timing(fontSize, {duration: 200, toValue: 1.30}),
+          Animated.spring(fontSize, {friction: 4, toValue: 1})
+        ]).start()
+
+        setTimeout(() => this.props.navigation.navigate('DeckInfo', {deck}), 350)
+
+    }
+
+    render() {
+        const {deck} = this.props;
+
+        return (
+            <DeckStyled>
+                <Animated.Text style={[{fontWeight: 'bold', fontSize: 30}, {transform: [{scale: this.state.fontSize}]}]} onPress={() => this.titlePressed()}>{deck.title}</Animated.Text>
+                <Subtitle>{cardQuantitiesLiteral(deck.questions.length)}</Subtitle>
+            </DeckStyled>
+        )
+    }
 }
 
 const DeckStyled = styled.View`
@@ -21,12 +44,6 @@ const DeckStyled = styled.View`
     border-bottom-color: black;
     border-bottom-width: 2;
 `
-
-const Title = styled.Text`
-    font-size: 30;
-    font-weight: bold;
-`
-
 
 const Subtitle = styled.Text`
     font-weight: bold;
